a0747ac507b9ef546601106828251a36973d8276
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-04-24 14:01:09 +0300

    Message state

--- src/Exceptions/StateIsNotOk.php
+++ src/Exceptions/StateIsNotOk.php
@@ -11,14 +11,18 @@ use Exception;
 final class StateIsNotOk extends Exception
 {
     protected Message $smsMsg;
+    private string $state;
 
-    public function __construct(Message $message)
+    public function __construct(Message $message,string $state)
     {
         $this->smsMsg = $message;
+        $this->state = $state;
         parent::__construct('Message state is not ok');
     }
 
-
+    public function getMessageState():string {
+        return $this->state;
+    }
     public function getSMSMessage(): Message
     {
         return $this->smsMsg;
