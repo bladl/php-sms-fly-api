29c7a107111c0f60f678ac08d71bd3739c06b8de
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-04-24 15:12:17 +0300

    logger improvements

--- src/SMSFlyAPI.php
+++ src/SMSFlyAPI.php
@@ -1,9 +1,8 @@
 <?php
+
 declare(strict_types=1);
 
-
 namespace BladL\SMSFly;
-
 
 use BladL\SMSFly\Data\MessagesResult;
 use BladL\SMSFly\Exceptions\CurlFailed;
@@ -11,34 +10,34 @@ use BladL\SMSFly\Exceptions\ErrorReturned;
 use BladL\SMSFly\Exceptions\StateIsNotOk;
 use BladL\SMSFly\Types\Campaign\StateCode;
 use BladL\Time\TimeZone;
-use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use SimpleXMLElement;
 
 class SMSFlyAPI
 {
+    use LoggerAwareTrait;
     private string $password;
     private string $login;
-    public static function getTimeZone():TimeZone {
+
+    public static function getTimeZone(): TimeZone
+    {
         return new TimeZone('Europe/Kiev');
     }
-    private LoggerInterface $logger;
 
-    final public function setLogger(LoggerInterface $logger): void
-    {
-        $this->logger = $logger;
-    }
     public function __construct(string $login, string $password)
     {
         $this->logger = new NullLogger();
         $this->login = $login;
         $this->password = $password;
     }
+
     /**
      * @throws CurlFailed|ErrorReturned|StateIsNotOk
      */
-    public function sendSms(Message $message):MessagesResult {
-        $result  = new MessagesResult($this->sendPost('SENDSMS',$message->getXmlBody()));
+    public function sendSms(Message $message): MessagesResult
+    {
+        $result = new MessagesResult($this->sendPost('SENDSMS', $message->getXmlBody()));
         $state = $result->getStateCode();
         if ($state->isOneOf(StateCode::ACCEPT)) {
             return $result;
@@ -46,36 +45,37 @@ class SMSFlyAPI
         if ($state->isOneOf(StateCode::INSUFFICIENT_FUNDS)) {
             $this->logger->alert('Insufficient funds!');
         } else {
-            $this->logger->error($state->toString().' '.$result->getStateDesc(),[$message->getRecipient()]);
+            $this->logger->error($state->toString().' '.$result->getStateDesc(), [$message->getRecipient()]);
         }
-        throw new StateIsNotOk($message,$state,$result->getStateDesc());
+        throw new StateIsNotOk($message, $state, $result->getStateDesc());
     }
 
     /**
      * @throws CurlFailed|ErrorReturned
      */
-    protected function sendPost(string $operation,string $body): SimpleXMLElement {
+    protected function sendPost(string $operation, string $body): SimpleXMLElement
+    {
         $post_fields =
             '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL
             ."<request><operation>$operation</operation>$body</request>";
         $this->logger->debug("POST '$post_fields'");
         $request = curl_init();
-        curl_setopt($request, CURLOPT_USERPWD , $this->login.':'.$this->password);
-        /** @noinspection CurlSslServerSpoofingInspection */
-        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($request, CURLOPT_USERPWD, $this->login.':'.$this->password);
+        /* @noinspection CurlSslServerSpoofingInspection */
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($request, CURLOPT_POST, 1);
-        /** @noinspection HttpUrlsUsage */
+        /* @noinspection HttpUrlsUsage */
         curl_setopt($request, CURLOPT_URL, 'http://sms-fly.com/api/api.php');
         curl_setopt($request, CURLOPT_HTTPHEADER, ['Content-Type: text/xml', 'Accept: text/xml']);
         curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($request, CURLOPT_POSTFIELDS,  $post_fields);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $post_fields);
         $response = curl_exec($request);
         $this->logger->debug("Response '$response'");
         curl_close($request);
         if (false === $response) {
             $error = curl_error($request);
             $this->logger->critical("Curl error '$error'");
-            throw new CurlFailed($error,curl_errno($request));
+            throw new CurlFailed($error, curl_errno($request));
         }
         $use_errors = libxml_use_internal_errors(true);
         $xml_resp = simplexml_load_string($response);
@@ -86,6 +86,5 @@ class SMSFlyAPI
         }
         $this->logger->error("Error '$response'");
         throw new ErrorReturned($response);
-
     }
 }
